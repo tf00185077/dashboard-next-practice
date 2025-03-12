7ef424fada97445f55a1f84112e3a9674fbb8789
Tim <tf00185077@gmail.com>
2025-03-12 18:41:30 +0800

    CRUD invoice ES Lint

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -1,5 +1,9 @@
 import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import { deleteInvoice } from '@/app/lib/action';
+// import postgres from 'postgres';
+// import { revalidatePath } from 'next/cache';
+// const sql = postgres(process.env.DATABASE_URL!);
 
 export function CreateInvoice() {
   return (
@@ -13,10 +17,10 @@ export function CreateInvoice() {
   );
 }
 
-export function UpdateInvoice({ id }: { id: string }) {
+export function UpdateInvoice({ id }: { id: string; }) {
   return (
     <Link
-      href="/dashboard/invoices"
+      href={`/dashboard/invoices/${id}/edit`}
       className="rounded-md border p-2 hover:bg-gray-100"
     >
       <PencilIcon className="w-5" />
@@ -24,13 +28,27 @@ export function UpdateInvoice({ id }: { id: string }) {
   );
 }
 
-export function DeleteInvoice({ id }: { id: string }) {
+export function DeleteInvoice({ id }: { id: string; }) {
+  const deleteInvoiceWithId = async () => {
+    'use server';
+    return await deleteInvoice(id);
+  }; //(O)
+
+  //  const deleteInvoiceWithId = async () => await deleteInvoice(id); //(X)
+  // const deleteInvoiceWithId = deleteInvoice.bind(null, id);
+  // const deleteInvoiceWithId = async () => {
+  //   'use server';
+  //   await sql`DELETE FROM invoices WHERE id = ${id}`;
+  //   revalidatePath('/dashboard/invoices');
+  // }; //(O)
   return (
     <>
-      <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
-        <span className="sr-only">Delete</span>
-        <TrashIcon className="w-5" />
-      </button>
+      <form action={deleteInvoiceWithId}>
+        <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
+          <span className="sr-only">Delete</span>
+          <TrashIcon className="w-5" />
+        </button>
+      </form>
     </>
   );
 }
